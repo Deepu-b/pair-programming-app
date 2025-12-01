849ab309058a1b0792ee2e0614ad21fac7838e62
Deepu-b <deepanshubudhraja@gmail.com>
2025-12-01 15:07:39 +0530

    added 60 sec room state sync with db

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,55 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  
 from app.api import endpoints
 from app.db.base import Base, engine
+import asyncio
+from contextlib import asynccontextmanager
 
-Base.metadata.create_all(bind=engine)
+from app.db.base import Base, engine, SessionLocal
+from app.services.socket_manager import manager
+from app.db.models import Room
 
-app = FastAPI(title="Pair Programming API")
+
+async def periodic_db_sync():
+    """
+    Runs every 60 seconds.
+    Iterates through active rooms in memory and flushes state to Postgres.
+    """
+
+    while True:
+        await asyncio.sleep(60)
+
+        db = SessionLocal()
+        try:
+            active_rooms = list(manager.room_state.keys())
+            print(active_rooms)
+            for room_id in active_rooms:
+                room_data = manager.room_state.get(room_id)
+                if not room_data: 
+                    continue
+                db_room = db.query(Room).filter(Room.room_id == room_id).first()
+                if db_room:
+                    db_room.code = current_code
+                
+            db.commit()
+        except Exception as e:
+            db.rollback()
+        finally:
+            db.close()
+
+@asynccontextmanager
+async def lifespan(app :FastAPI):
+    Base.metadata.create_all(bind=engine)
+    sync_task = asyncio.create_task(periodic_db_sync())
+
+    yield
+
+    sync_task.cancel()
+    try:
+        await sync_task
+    except asyncio.CancelledError:
+        pass
+
+app = FastAPI(title="Pair Programming API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
